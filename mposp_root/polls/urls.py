b61f8acb4d97ded9231f0dc9a0972a28aa6a13e7
Hans Hendrickx <hhlhendrickx@gmail.com>
2019-12-10 08:17:04 +0100

    Parrt 5 tests.py
    works well

--- mposp_root/polls/urls.py
+++ mposp_root/polls/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = 'polls'
 
+# detail in line 10 is not there. That has to be like that!
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
